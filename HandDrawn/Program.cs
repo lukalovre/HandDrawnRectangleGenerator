eb3edcfd5b6a5694a98185107440cc14da9bc8bf
lukalovre <lukalovre92@gmail.com>
2019-07-31 21:19:01 +0200

    renamed form1 to Main

--- HandDrawn/Program.cs
+++ HandDrawn/Program.cs
@@ -13,7 +13,7 @@ namespace HandDrawn
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
