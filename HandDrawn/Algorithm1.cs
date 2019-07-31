0619109a1711691fd13ff5e4e632d09c90bc6e4f
lukalovre <lukalovre92@gmail.com>
2019-07-31 23:17:15 +0200

    Added draw tools

--- HandDrawn/Algorithm1.cs
+++ HandDrawn/Algorithm1.cs
@@ -4,22 +4,15 @@ namespace HandDrawn
 {
     public static class Algorithm1
     {
-        private static int maxDeviation = 30;
-
-        private static readonly Brush s_brush = new SolidBrush(Color.Black);
-
-        private static int s_brushSize = 2;
-
-
         public static void Draw(int width, int height)
         {
-            using(Bitmap bitmap = new Bitmap(width, height + 2 * maxDeviation))
+            using(Bitmap bitmap = new Bitmap(width, height + 2 * DrawTools.MaxDeviation))
             {
                 using(Graphics graphics = Graphics.FromImage(bitmap))
                 {
                     for(int x = 0; x < width; x++)
                     {
-                        graphics.FillRectangle(s_brush, x, maxDeviation, s_brushSize, s_brushSize);
+                        graphics.FillRectangle(DrawTools.Brush, x, DrawTools.MaxDeviation, DrawTools.BrushSize, DrawTools.BrushSize);
                     }
                 }
 
---
+++ HandDrawn/DrawTools.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace HandDrawn
+{
+    public static class DrawTools
+    {
+
+        public static int MaxDeviation = 30;
+
+        public static readonly Brush Brush = new SolidBrush(Color.Black);
+
+        public static readonly int BrushSize = 2;
+    }
+}
